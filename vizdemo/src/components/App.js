a8926a675e350942d1cad9a55c34145696eb7b08
Julian Frattini <julian.frattini@alice-dsl.net>
2023-01-05 14:00:52 +0100

    Repackaged and solved rendering issue

--- vizdemo/src/components/App.js
+++ vizdemo/src/components/App.js
@@ -1,12 +1,14 @@
 import '../styles/App.css';
 
-import LabelVisualizer from './LabelVisualizer'
+import LabelVisualizer from '../packages/labelvisualization/LabelVisualizer'
+import mocklabels from "../util/mocklabels"
 
 function App() {
+
   return (
     <div className="App">
       <h1>Label Visualization Demo</h1>
-      <LabelVisualizer text="If the red button is pushed the system shuts down."></LabelVisualizer>
+      <LabelVisualizer text="If the red button is pushed the system shuts down." labels={mocklabels}></LabelVisualizer>
     </div>
   );
 }
--- vizdemo/src/packages/labelvisualization/LabelVisualizer.js
+++ vizdemo/src/packages/labelvisualization/LabelVisualizer.js
@@ -1,11 +1,13 @@
-import Annotation from "./Annotation";
-
-import mocklabels from "../util/mocklabels"
+import Annotation from "./components/Annotation";
 
 import React, { useState, useEffect } from 'react'
 
-function LabelVisualizer({ text }) {
-    const [labels, setLabels] = useState([])
+function LabelVisualizer({ text, labels }) {
+    const [renderlabels, setRenderlabels] = useState([])
+
+    useEffect( () => {
+        setRenderlabels(labels);
+     }, []);
 
     const getposition = (index) => {
         var sen = document.getElementById("sentence")
@@ -13,18 +15,14 @@ function LabelVisualizer({ text }) {
         return parseInt(position)
     }
 
-    useEffect(() => {
-        setLabels(mocklabels)
-    }, []);
-
     return (
         <svg width="800" height="60">
-            {
-                labels.map((item, index) => {
+            <text id="sentence" x="5" y="55" fill="black">{text}</text>
+            { 
+                renderlabels.map((item, index) => {
                     return <Annotation key={index} x={getposition(item.start)} y="40" width={getposition(item.end) - getposition(item.start)} type={item.type}></Annotation>
                 })
-            }
-            <text id="sentence" x="5" y="55" fill="black">{text}</text>
+            } 
         </svg>
     );
 }
